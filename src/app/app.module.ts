9b93c0fa78b34927a209a91f4edd7b8fa7c95d76
CarinaKarthaus <carina.karthaus@gmail.com>
2020-09-05 20:58:59 +0200

    Creation of projects-list using *ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { MenuComponent } from './menu/menu.component';
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  projects = [
+    { 
+      title: 'El Pollo Loco', 
+      details: 'A JavaScript-based jump-and-run-game.', 
+      app_link: 'http://carina-karthaus.developerakademie.com/el_pollo_loco/', 
+      github_link: 'https://github.com/CarinaKarthaus/el_pollo_loco', 
+      img: 'el_pollo_loco.png',
+      img_mobile: ''
+    },
+    {
+      title: 'JOIN - The Task Manager', 
+      details: 'An app to create, assign and manage tasks using Eisenhower\'s urgent-important-principle.', 
+      app_link: 'http://carina-karthaus.developerakademie.com/task-management/', 
+      github_link: 'https://github.com/CarinaKarthaus/task-management', 
+      img: 'join.png',
+      img_mobile: 'join_mobile.png'
+    }    
+  ]
+
+ 
   constructor() { }
 
   ngOnInit(): void {
